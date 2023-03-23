1bd338aba91aafb9cd772859b7f25646c02d5041
LupulescuAlexandru <loopiedooo@gmail.com>
2023-03-23 13:51:46 +0200

    init

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {useEffect, useState} from "react";
+import * as Location from 'expo-location';
+import RNSpeedometer from 'react-native-speedometer';
+import {LocationAccuracy} from "expo-location";
+
+const convertMphToKmh = (speed) => {
+  return speed * 3.6
+}
 
 export default function App() {
+  const [speed, setSpeed] = useState(0);
+
+  useEffect(() => {
+    const fetchSpeed = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        return;
+      }
+      await Location.watchPositionAsync({accuracy: LocationAccuracy.BestForNavigation, distanceInterval: 5}, (location) => {
+        setSpeed(convertMphToKmh(location.coords.speed));
+      });
+    }
+    void fetchSpeed();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RNSpeedometer value={speed} size={200} maxValue={200} labelNoteStyle={{color: 'white'}}/>
     </View>
   );
 }
