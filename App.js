c373d9fd09ff2d4ee9568be84fd11e1717f23896
LupulescuAlexandru <loopiedooo@gmail.com>
2023-03-26 22:17:37 +0300

    sync

--- App.js
+++ App.js
@@ -1,41 +1,20 @@
-import { StyleSheet, View } from 'react-native';
-import {useEffect, useState} from "react";
-import * as Location from 'expo-location';
-import RNSpeedometer from 'react-native-speedometer';
-import {LocationAccuracy} from "expo-location";
+import MapScreen from "./src/screens/MapScreen/MapScreen";
+import HistoryScreen from "./src/screens/HistoryScreen/HistoryScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
-const convertMphToKmh = (speed) => {
-  return speed * 3.6
-}
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [speed, setSpeed] = useState(0);
-
-  useEffect(() => {
-    const fetchSpeed = async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        return;
-      }
-      await Location.watchPositionAsync({accuracy: LocationAccuracy.BestForNavigation, distanceInterval: 5}, (location) => {
-        setSpeed(convertMphToKmh(location.coords.speed));
-      });
-    }
-    void fetchSpeed();
-  }, [])
-
   return (
-    <View style={styles.container}>
-      <RNSpeedometer value={speed} size={200} maxValue={200} labelNoteStyle={{color: 'white'}}/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+          headerShown: false,
+          unmountOnBlur: true
+      }}>
+        <Tab.Screen name="Istoric" component={HistoryScreen} />
+        <Tab.Screen name="Harta" component={MapScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
